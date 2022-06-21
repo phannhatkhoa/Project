cf060bda9037beb4352b6b59b2c03b619c2b9a45
phannhatkhoa <khoapngcd201807@fpt.edu.vn>
2022-06-21 19:25:38 +0700

    asdsaa

---
+++ src/Entity/Product.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $Category;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Price;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $Weight;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->Category;
+    }
+
+    public function setCategory(string $Category): self
+    {
+        $this->Category = $Category;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Decription): self
+    {
+        $this->Description = $Decription;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(int $Price): self
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getWeight(): ?float
+    {
+        return $this->Weight;
+    }
+
+    public function setWeight(float $Weight): self
+    {
+        $this->Weight = $Weight;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(string $Image): self
+    {
+        $this->Image = $Image;
+
+        return $this;
+    }
+}
